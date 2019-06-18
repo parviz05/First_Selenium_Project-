edf11910c33fca2187221fd75437c0a05da057c9
parviz05 <alas.parviz@gmail.com>
2019-06-18 16:02:03 -0500

    firs commit

---
+++ src/com/Parviz/MyClass.java
@@ -0,0 +1,23 @@
+package com.Parviz;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class MyClass {
+
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver","/Users/parvizalasgarov/Documents/SeleniumDependecies/drivers/chromedriver");
+        WebDriver driver =new ChromeDriver();
+
+        driver.get("https://www.google.com/");
+//
+//        System.setProperty("webdriver.gecko.driver","/Users/parvizalasgarov/Documents/SeleniumDependecies/drivers/geckodriver");
+//        WebDriver driver =new FirefoxDriver();
+//
+//        driver.get("https://www.google.com/");
+
+    }
+}
+
