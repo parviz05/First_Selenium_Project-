521c4fcdf6c4bef550f8e231340654d89431e489
parviz05 <alas.parviz@gmail.com>
2019-06-19 13:37:12 -0500

    new statement added in new class

--- src/com/Parviz/MySecondDay.java
+++ src/com/Parviz/MySecondDay.java
@@ -5,6 +5,7 @@ public class MySecondDay {
     public static void main(String[] args) {
 
         System.out.println("the day is looking fine !");
+        System.out.println("what do u think about the day?");
 
     }
 }
